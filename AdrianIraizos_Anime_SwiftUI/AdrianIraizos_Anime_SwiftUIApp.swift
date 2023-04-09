210f9bf1a7ef149504f0d00bfd4e8593ae960640
adrian <adrian@iraizos.me>
2023-04-09 17:58:25 +0200

    Initial Commit

---
+++ AdrianIraizos_Anime_SwiftUI/AdrianIraizos_Anime_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdrianIraizos_Anime_SwiftUIApp.swift
+//  AdrianIraizos_Anime_SwiftUI
+//
+//  Created by Adrian Iraizos Mendoza on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AdrianIraizos_Anime_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
